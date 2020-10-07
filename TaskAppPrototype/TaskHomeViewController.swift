5008da2aa5f04f6fc6d3a7d2e275333961891448
Casi Barden <casibarden@gmail.com>
2020-10-06 23:32:31 -0500

    Refactored Reroll Code

--- TaskAppPrototype/TaskHomeViewController.swift
+++ TaskAppPrototype/TaskHomeViewController.swift
@@ -14,9 +14,7 @@ class taskHomeViewController: UIViewController {
     
     
     let listOfTasks = ["Write 5 things you love about yourself", "Drink a cup of herbal tea", "Go for a long walk", "Send a message to someone you love", "Call someone you love", "Light your favorite candle", "Cook your favorite meal", "Hug someone", "Drink a full glass of water", "Tidy up one small space (desk, drawer, etc.)","Re-arrange a room in your home", "Get out into nature", "Send a surprise card or care package to someone", "Put the dishes away"]
-        
     
- 
     
     @IBOutlet weak var taskBoxOne: UILabel!
     
@@ -24,59 +22,53 @@ class taskHomeViewController: UIViewController {
     
     @IBOutlet weak var taskBoxThree: UILabel!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        updateFirstTask()
-        updateSecondTask()
-        updateThirdTask()   
-        
-    }
-    
-
-    
-    @IBAction func rerollFirstTaskButton(_ sender: UIButton) {
-        //Rerolls first task
-        updateFirstTask()
-    }
-    
-    
-    func updateFirstTask(){
+    func generateRandomTask(){
         
         randomTask = Int(arc4random_uniform(UInt32(listOfTasks.count)))
         
+    }
+    
+    func generateInitialTasks(){
+        
+        generateRandomTask()
         self.taskBoxOne.text = listOfTasks[randomTask]
-    }
-    
-    
-    
-    
-    @IBAction func rerollButtonTwoTapped(_ sender: UIButton) {
-        //Rerolls second task
-        updateSecondTask()
-    }
-    
-    
-    func updateSecondTask(){
-        
-        randomTask = Int(arc4random_uniform(UInt32(listOfTasks.count)))
-        
+        generateRandomTask()
         self.taskBoxTwo.text = listOfTasks[randomTask]
-        
-    }
-    
-    @IBAction func rerollButtonThreeTapped(_ sender: UIButton) {
-        //Rerolls third task
-        updateThirdTask()
-    }
-    
-    func updateThirdTask(){
-        
-        randomTask = Int(arc4random_uniform(UInt32(listOfTasks.count)))
-        
+        generateRandomTask()
         self.taskBoxThree.text = listOfTasks[randomTask]
         
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generateInitialTasks()
+        
+    }
+
+    
+    @IBAction func rerollFirstTaskButton(_ sender: UIButton) {
+        //Rerolls first task
+        generateRandomTask()
+        self.taskBoxOne.text = listOfTasks[randomTask]
+    }
+    
+    @IBAction func rerollButtonTwoTapped(_ sender: UIButton) {
+        //Rerolls second task
+        generateRandomTask()
+        self.taskBoxTwo.text = listOfTasks[randomTask]
+    }
+    
+    @IBAction func rerollButtonThreeTapped(_ sender: UIButton) {
+        //Rerolls third task
+        generateRandomTask()
+        self.taskBoxThree.text = listOfTasks[randomTask]
+    }
+    
+    
+    
+    
+    
+    
 }
+
